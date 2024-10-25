29d256a47c1aa681b9ce33b651e7a7393019b544
ikumi <ikumin518@gmail.com>
2024-10-25 19:08:32 +0900

    10/25

--- archive.php
+++ archive.php
@@ -1,76 +1,6 @@
-<!DOCTYPE html>
-<html lang="ja">
-  <head>
-    <meta charset="utf-8">
-    <title>ダミーサイト</title>
-    <meta name="description" content="アメリカ本場の味を再現したハンバーガーショップ">
-    <!-- viewport設定 -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- ファビコン -->
-    <link rel="icon" type="image/png" href="img/favicon.png">
-    <!-- リセットCSS
-    <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css"> -->
-    <!-- googleフォント -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p&display=swap" rel="stylesheet">
-    <!-- css -->
-    <link rel="stylesheet" href="/css/style.min.css">
-  </head>
+<?php get_header(); ?>
 
-  <body class="u-container">
-    <!-- header -->
-    <header class="l-header">
-      <h1 class="c-title__header"><a href="#">Hamburger</a></h1>
-      <div class="c-form__mark">
-        <input type="search" name="search" class="c-form__keyword">
-        <input type="submit" name="submit" value="検索" class="c-form__button">
-      </div>
-    </header>
-    <!-- サイドバーのメニュー -->
-    <aside class="l-nav">
-      <button class="c-button js-btn">
-        <span class="c-button-line c-button-line1"></span>
-        <span class="c-button-line2">Menu</span>
-        <span class="c-button-line c-button-line3"></span>
-      </button>
-      <nav class="p-navi">
-        <p class="c-title__navi">Menu</p>
-        <ul class="p-navi__content js-menu" aria-hidden="true">
-          <li class="c-title__navi-ttl"><h3><a href="#">バーガー</a></h3>
-            <ul class="c-navi">
-              <li><a href="#">ハンバーガー</a></li>
-              <li><a href="#">チーズバーガー</a></li>
-              <li><a href="#">テリヤキバーガー</a></li>
-              <li><a href="#">アボカドバーガー</a></li>
-              <li><a href="#">フィッシュバーガー</a></li>
-              <li><a href="#">ベーコンバーガー</a></li>
-              <li><a href="#">チキンバーガー</a></li>
-            </ul>
-          </li>
-          <li class="c-title__navi-ttl"><h3><a href="#">サイド</a></h3>
-            <ul class="c-navi">
-              <li><a href="#">ポテト</a></li>
-              <li><a href="#">サラダ</a></li>
-              <li><a href="#">ナゲット</a></li>
-              <li><a href="#">コーン</a></li>
-            </ul>
-          </li>
-          <li class="c-title__navi-ttl"><h3><a href="#">ドリンク</a></h3>
-            <ul class="c-navi">
-              <li><a href="#">コーラ</a></li>
-              <li><a href="#">ファンタ</a></li>
-              <li><a href="#">オレンジ</a></li>
-              <li><a href="#">アップル</a></li>
-              <li><a href="#">紅茶（Ice/Hot）</a></li>
-              <li><a href="#">コーヒー（Ice/Hot）</a></li>
-            </ul>
-          </li>
-        </ul>
-      </nav>
-      <div class="p-navi-bg js-bg-black"></div>  <!--背景黒-->
-    </aside>
+<?php get_sidebar(); ?>
 
     <!-- main -->
     <main class="u-main__container">
@@ -91,19 +21,29 @@
             テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
         </div>
         <section class="p-lists">
+          <?php $args = array(
+            'posts_per_page' => 8         //読み込む記事数
+            );
+          $posts = get_posts( $args );    //配列で指定した内容で取得
+          foreach( $posts as $post ):     //ループ処理で1件ずつ表示
+            setup_postdate( $post ); ?>     <!-- the_titleなどで記事情報を表示できるようにする -->
           <figure class="p-list">
-            <img class="img-list" src="./img/menu-ham.jpg" alt="チーズバーガーの写真1">
+            <img class="img-list" src="<?php the_post_thumbnail_url( 'medium' ); ?>" alt="チーズバーガーの写真">
             <figcaption class="c-caption">
-              <h3>チーズバーガー</h3>
+              <h3><?php the_title(); ?></h3>
               <h4>こだわり</h4>
               <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
                 テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
                 テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
-              <p><a class="c-button__menu" href="#">詳しく見る</a></p>
+              <p><a class="c-button__menu" href="<?php the_permalink(); ?>">詳しく見る</a></p>
             </figcaption>
           </figure>
+          <?php endforeach; ?>
+          <?php wp_reset_postdate(); ?>         <!-- 使用データをリセットして取得処理終了 -->
+
+
           <figure class="p-list">
-            <img src="./img/menu-ham.jpg" alt="チーズバーガーの写真2">
+            <img src="./img/menu-ham.jpg" alt="ダブルチーズバーガーの写真">
             <figcaption class="c-caption">
               <h3>ダブルチーズバーガー</h3>
               <h4>こだわり</h4>
@@ -114,7 +54,7 @@
             </figcaption>
           </figure>
           <figure class="p-list">
-            <img src="./img/menu-ham.jpg" alt="チーズバーガーの写真3">
+            <img src="./img/menu-ham.jpg" alt="スペシャルチーズバーガーの写真">
             <figcaption class="c-caption">
               <h3>スペシャルチーズバーガー</h3>
               <h4>こだわり</h4>
--- functions.php
+++ functions.php
@@ -1,7 +1,19 @@
 <?php
 
 add_theme_support( 'menus' );
-add_theme_support( 'post-thumbnails' );  //アイキャッチの有効化
+add_theme_support( 'title-tag' );         // タイトルタグ
+add_theme_support( 'post-thumbnails' );   //アイキャッチの有効化
+
+//タイトル出力
+function hum_title ( $title ) {
+  if ( is_front_page() && is_home() ) {   //トップページなら
+    $title = get_bloginfo( 'name', 'display' );
+  } elseif ( is_singular() ) {   //シングルページなら
+    $title = single_post_title( '', false );
+  }
+  return $title;
+}
+add_filter( 'pre_get_document_title', 'hum_title' );
 
 // 不要なjQueryを削除する
 function my_scripts() {
@@ -11,7 +23,7 @@ add_action( 'wp_enqueue_scripts', 'my_scripts' );
 
 // CSS, JavaScript読み込み
 function my_enqueue_styles() {
-  wp_enqueue_style( 'style', get_remplate_directory_uri() . '/css/style.css', array(), '1.0.0', 'all');
+  wp_enqueue_style( 'style', get_template_directory_uri() . '/css/style.css', array(), '1.0.0', 'all');
   wp_enqueue_script( 'js', get_template_derectory_uri() . '/js/nav.js', array( 'jquery' ), '1.0.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'my_enqueue_styles' );
